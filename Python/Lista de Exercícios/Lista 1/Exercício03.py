02ae6640a0e66fb271fa984da40508bf6f487b9c
Bruno Serpa <79608249+BrunoSerpa@users.noreply.github.com>
2023-02-12 09:38:03 -0300

    Add files via upload

---
+++ Python/Lista de Exercícios/Lista 1/Exercício03.py
@@ -0,0 +1,7 @@
+#03) Escreva um programa que leia a quantidade de dias, horas, minutos e segundos do usuário. Calcule o total em segundos.
+dias = int(input("insira um número de dias\n resposta:"))
+horas = int(input("insira um número de horas\n resposta:"))
+minutos = int(input("insira um número de minutos\n resposta:"))
+segundos = int(input("insira um número de segundos\n resposta:"))
+segundosTotais = ((dias * 24 + horas) * 60 + minutos) * 60 + segundos
+print('Isso tudo é igual a {} segundos'.format(segundosTotais))
